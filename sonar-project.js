8c1742d1972d1852633734efe429de44893e25f7
smadwara <108672423+smadwara@users.noreply.github.com>
2023-03-21 17:24:23 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.237.172.42:9000/',
+  serverUrl: 'http://3.218.244.104:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
